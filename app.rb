ea89fed0afa7f316ae6d985d53196647d2ff3e83
Ilya Skurikhin <ilya@skurikhin.ch>
2017-01-26 21:31:09 +0100

    made code better

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'rubygems'
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
-  g = Git.open('~/Documents/webhook-test/', :log => Logger.new(STDOUT))
+  g = Git.open(Dir.pwd, :log => Logger.new(STDOUT))
   g.pull
 end
 
