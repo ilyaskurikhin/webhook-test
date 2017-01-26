5b367e75646deedc7a4c49f6b3b64a055d9ceeb3
Ilya Skurikhin <ilya@skurikhin.ch>
2017-01-26 21:49:54 +0100

    prefected app

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 
 post '/payload' do
   push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  puts "Recieved POST from GitHub, pulling..."
   g = Git.open(Dir.pwd, :log => Logger.new(STDOUT))
   g.pull
 end
