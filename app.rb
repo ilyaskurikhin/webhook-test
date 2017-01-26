41f84949dafb3a826579d1ecc681613059d9bd18
Ilya Skurikhin <ilya@skurikhin.ch>
2017-01-26 18:17:53 +0100

    add git functionality

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 require 'json'
+require 'git'
+
 
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
+  g = Git.open(working_dir, :log => Logger.new(STDOUT))
+  g.pull
 end
+
